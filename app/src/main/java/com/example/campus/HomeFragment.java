ddf5373e2ac52dc60b6a261fabc3c8e83420f4b9
tommykang <tommykang@tencent.com>
2022-03-05 20:57:06 +0800

    feature:增加底部导航栏

---
+++ app/src/main/java/com/example/campus/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.campus;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import android.view.*;
+import android.widget.TextView;
+
+
+public class HomeFragment extends Fragment {
+    private String mFrom;
+    static HomeFragment newInstance(String from){
+        HomeFragment fragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("from",from);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getArguments()!=null){
+            mFrom = getArguments().getString("from");
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment_layout,null);
+        TextView textView = (TextView) view.findViewById(R.id.title_from);
+        TextView content = (TextView) view.findViewById(R.id.fragment_content);
+        textView.setText(mFrom);
+        content.setText("Homefragment");
+        return view;
+    }
+}
